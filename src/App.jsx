7ef883c9e897a93dda3c12150e89f88757c3a75b
ayush_K <pritimasuman09@gmail.com>
2026-08-25 23:49:56 +0530

    Add empty line before closing Routes tag

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/chat" element={<Chat />} />
+      
     </Routes>
   )
 }
